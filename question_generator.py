6dd2896e14653db8847b0276b40976031ae21f9d
Dylson14 <dylsonuni@gmail.com>
2025-10-20 20:55:30 +0100

    fixed infinite loop error, practiced match cases

--- question_generator.py
+++ question_generator.py
@@ -2,20 +2,33 @@ import os
 os.system('cls' if os.name == 'nt' else 'clear') # clears terminal when running Python Script
 
 questions_list = [] # [("question", True/False)] list contains questions and their respective answers
-questions_list.append(("question1", True))
-questions_list.append(("question2", False))
 
-user_action = input("To add a question, type add: ")
-user_action = user_action.strip()
 
 while True:
+    user_action = input("To add a question, type add or show: ")
+    user_action = user_action.strip()
+    
     match user_action:
         case 'add':
             print("You've entered the add case")
             
             user_question = input("Write your question: ").strip()
-            boolean_answer = input("Is your question True or False").strip()
+            boolean_answer = input("Is your question True or False: ").strip()
             
             questions_list.append((user_question, boolean_answer))
+            print(questions_list)
+            
+            print("Exiting case")
+            break
+        case 'show':
+            print("Entered show case")
+            continue
+        case _:
+            print("Your command is invalid, try again")
+            continue
+    
+    print("End of While Loop")
+    
+print("We're outside the while loop")
             
             
