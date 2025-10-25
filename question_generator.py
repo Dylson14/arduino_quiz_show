00890e153b75ea4559857129688d656ecef29a6a
Dylson14 <dylsonuni@gmail.com>
2025-10-25 10:43:00 +0100

    created functions for reading and writing questions

--- question_generator.py
+++ question_generator.py
@@ -1,11 +1,17 @@
 import os
 os.system('cls' if os.name == 'nt' else 'clear') # clears terminal when running Python Script
 
-questions_list = [] # [("question", True/False)] list contains questions and their respective answers
+def get_questions(filepath):
+    with open(filepath,'r') as file:
+                questions_list_local = file.readlines() # returns list
+                return questions_list_local
 
+def write_questions(filepath, questions):
+    with open(filepath,'w') as file:
+                file.writelines(questions) # performs write() method on each list item
 
 while True:
-    user_action = input("Type add, show, remove, exit or start ")
+    user_action = input("Type add, show, remove, exit or start: ")
     user_action = user_action.strip()
     
     match user_action:
@@ -13,12 +19,24 @@ while True:
             user_question = input("Write your question: ").strip()
             boolean_answer = input("Is your question True or False: ").strip()
             
-            questions_list.append((user_question, boolean_answer))
+            questions_list = get_questions('questions_list.txt')
+            
+            questions_list.append(str((user_question, boolean_answer)))
+            
+            write_questions('questions_list.txt', questions_list)
+                
         case 'show':
-            print(questions_list)
+            questions_list = get_questions('questions_list.txt')
+                
+            print(questions_list) 
+            
+        case 'start':
+            questions_list = get_questions('questions_list.txt')
+            
         case 'exit':
             print('leaving quiz, thanks for playing')
             break
+        
         case _:
             print("Your command is invalid, try again")
     
